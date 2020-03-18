802335bfb9d182424870ba3e3f0d1ec38ce72945
Pavel <shipilopavel92@gmail.com>
2020-03-18 09:26:11 +0300

    добавил в функции возможность задать время ожидания

--- search-field-plus-news-link-test.spec.js
+++ search-field-plus-news-link-test.spec.js
@@ -2,21 +2,25 @@ describe('Test from first Selenium task', function() {
   it('Search field and News link test', async function() {
     let searchField = await element(by.id("orb-search-q"));
 
-    async function waitForElementIsEnabled(elementName) {
+    async function waitForElementIsEnabled(elementName,maxTimeToWait) { //maxTimeToWait - максимальное время ожидания в мс
       let enabled;
+      let timePassed = 0;
   
-      while(!enabled) {
-          await browser.sleep(1000);
+      while(!enabled && timePassed < maxTimeToWait) {
           enabled = await elementName.isEnabled();
+          timePassed += 1000;
+          await browser.sleep(1000);
       } 
   }
 
-    async function waitForElementIsDisplayed(elementName) {
+    async function waitForElementIsDisplayed(elementName, maxTimeToWait) {
       let displayed;
+      let timePassed = 0;
 
-      while(!displayed) {
-          await browser.sleep(1000);
+      while(!displayed && timePassed < maxTimeToWait) {
           displayed = await elementName.isDisplayed();
+          timePassed += 1000;
+          await browser.sleep(1000);
       } 
   }
 
